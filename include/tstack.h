9d3b98ace994ec7a138071369672ffd89b313a8f
Tatiana-Lev <tanklevitan21@mail.ru>
2026-04-20 22:47:13 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,36 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T data[Size];
+    size_t topIndex;
+public:
+    TStack(): topIndex(0){}
+
+    void push(const T& value) {
+        if (topIndex < Size) {
+            data[topIndex++] = value;
+        }
+    }
+
+    T pop() {
+        if (topIndex > 0) {
+            return data[--topIndex];
+        }
+        return T();
+    }
+    T top() const {
+        if (topIndex > 0) {
+            return data[topIndex - 1];
+        }
+        return T();
+    }
+    bool empty() const {
+        return topIndex == 0;
+    }
+    bool full() const {
+        return topIndex >= Size;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
